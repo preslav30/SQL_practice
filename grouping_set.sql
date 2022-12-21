9e7f1c36e8eb5355ed34a2c7e8fe9f449a50960a
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-12-21 13:21:11 +0200

    final results

--- grouping_set.sql
+++ grouping_set.sql
@@ -4,8 +4,29 @@
 *  Table: Employees
 */
 
+SELECT de.dept_no, count(de.emp_no)
+FROM public.employees AS e
+INNER JOIN public.dept_emp AS de USING(emp_no)
+GROUP BY 
+    GROUPING SETS (
+        (de.dept_no),
+        ()
+    )
+ORDER BY de.dept_no ASC;
+
+
 /*
 *  Calculate the total average salary per department and the total using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
+
+SELECT de.dept_no, AVG(s.salary)
+FROM public.dept_emp AS de
+INNER JOIN public.salaries AS s USING(emp_no)
+GROUP BY
+    GROUPING SETS (
+    (), 
+    (de.dept_no)
+    )
+ORDER BY de.dept_no
