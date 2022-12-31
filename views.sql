570fe4ec5486a1510671168e9aa4aab97b06065b
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-12-31 11:35:07 +0200

    initializing file

---
+++ views.sql
@@ -0,0 +1,13 @@
+/*
+*  Create a view "90-95" that:
+*  Shows me all the employees, hired between 1990 and 1995
+*  Database: Employees
+*/
+
+
+
+/*
+*  Create a view "bigbucks" that:
+*  Shows me all employees that have ever had a salary over 80000
+*  Database: Employees
+*/
