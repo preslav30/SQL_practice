4b2462f79c29044b8aa019ae75561e5296c69a47
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-12-31 13:56:18 +0200

    final file

--- views.sql
+++ views.sql
@@ -4,6 +4,13 @@
 *  Database: Employees
 */
 
+CREATE VIEW "90-95" AS
+SELECT emp_no, first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT (YEAR FROM hire_date) BETWEEN 1990 AND 1995
+GROUP BY e.emp_no;
+
+SELECT * FROM "90-95";
 
 
 /*
@@ -11,3 +18,11 @@
 *  Shows me all employees that have ever had a salary over 80000
 *  Database: Employees
 */
+
+CREATE VIEW "bigbucks" AS
+SELECT e.*, s.salary
+FROM employees AS e 
+INNER JOIN salaries AS s ON e.emp_no = s.emp_no
+WHERE salary > 80000
+
+SELECT * FROM "bigbucks";
