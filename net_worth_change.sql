e336e2daadf79f36722a2955ebb2a12f4a37836c
preslav30 <112109330+preslav30@users.noreply.github.com>
2023-01-18 20:35:06 +0200

    solved

---
+++ net_worth_change.sql
@@ -0,0 +1,24 @@
+CREATE TABLE transactions (sender, receiver, amount, transaction_date) 
+AS (VALUES 
+(5, 2, 10, CAST('2-12-20' AS date)),
+(1, 3, 15, CAST('2-13-20' AS date)), 
+(2, 1, 20, CAST('2-13-20' AS date)), 
+(2, 3, 25, CAST('2-14-20' AS date)), 
+(3, 1, 20, CAST('2-15-20' AS date)), 
+(3, 2, 15, CAST('2-15-20' AS date)), 
+(1, 4, 5, CAST('2-16-20' AS date)));
+
+CREATE VIEW send AS
+SELECT sender, sum(amount) AS sendings
+FROM transactions
+GROUP BY sender;
+
+CREATE VIEW receive AS
+SELECT receiver, sum(amount) AS receivings
+FROM transactions
+GROUP BY receiver;
+
+SELECT COALESCE(s.sender, r.receiver) AS USER, (r.receivings - s.sendings) AS net_amount
+FROM send AS s
+FULL JOIN receive AS r ON s.sender = r.receiver
+ORDER BY net_amount DESC
