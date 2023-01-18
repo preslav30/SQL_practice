64a6d9fc737ade9166e58ebd03e2d3ac4dfb429e
preslav30 <112109330+preslav30@users.noreply.github.com>
2023-01-18 20:35:49 +0200

    end

--- net_worth_change.sql
+++ net_worth_change.sql
@@ -1,3 +1,8 @@
+-- From the following table of transactions between two users, 
+-- write a query to return the change in net worth for each user, 
+-- ordered by decreasing net change.
+
+
 CREATE TABLE transactions (sender, receiver, amount, transaction_date) 
 AS (VALUES 
 (5, 2, 10, CAST('2-12-20' AS date)),
@@ -18,7 +23,7 @@ SELECT receiver, sum(amount) AS receivings
 FROM transactions
 GROUP BY receiver;
 
-SELECT COALESCE(s.sender, r.receiver) AS USER, (r.receivings - s.sendings) AS net_amount
+SELECT COALESCE(s.sender, r.receiver) AS "user", COALESCE(r.receivings, 0) - COALESCE(s.sendings, 0) AS net_amount
 FROM send AS s
 FULL JOIN receive AS r ON s.sender = r.receiver
 ORDER BY net_amount DESC
